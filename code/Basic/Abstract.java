c278a9c75aa357c2aa4ad0f6e61ca2026671a4e4
Pushtikansara <pushti612@gmail.com>
2025-08-17 00:42:46 +0530

    final commit

---
+++ code/Basic/Abstract.java
@@ -0,0 +1,18 @@
+package Basic;
+abstract class animal
+{
+    public abstract void colour();
+    public void sound(){
+        System.out.println("sound");
+    }
+}
+class cat extends animal{
+    public void colour{
+        System.out.println("Cat is white");
+    }
+    
+
+}
+public class Abstract {
+    
+}
