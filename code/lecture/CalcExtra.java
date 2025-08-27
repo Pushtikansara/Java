56f127c535457b3d79cabe9696459c749f08fb19
Pushtikansara <pushti612@gmail.com>
2025-08-27 15:13:29 +0530

    final commit

--- code/lecture/CalcExtra.java
+++ code/lecture/CalcExtra.java
@@ -1,31 +1,30 @@
-package calc;
 
 
- public class Addition
+  class Addition
     {
         public int add(int a,int b)
         {
         return a+b;
         }
     }
-// public class Subtraction
-//  {
-//     public int sub(int a,int b)
-//     {
-//         return a-b;
-//     }
-// }
-// public class Muliplicatio{
-//      public int mul(int a,int b)
-//     {
-//         return a*b;
-//     }
-// }
-// public class Division{
+class Subtraction
+ {
+    public int sub(int a,int b)
+    {
+        return a-b;
+    }
+}
+class Multiplication{
+     public int mul(int a,int b)
+    {
+        return a*b;
+    }
+}
+ class Division{
    
-//     public float div(int a,int b)
-//     {
-//         return a/b;
-//     }
+    public float div(int a,int b)
+    {
+        return a/b;
+    }
 
-// }
+}
